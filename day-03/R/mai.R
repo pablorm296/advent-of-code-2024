092a51b23d992ce880c3bb106cca79ea51171e30
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2024-12-19 17:36:42 -0600

    Add day 03 part 1 solution

---
+++ day-03/R/mai.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+# Part 2 ======================================================================
+
+# Read the input file
+lines <- read_lines("./day-03/input/1.txt")
+
+# Concatenate the lines into a single character string
+corrupted_memory <- str_c(lines, collapse = "")
+
+mul_instructions <- str_extract_all(corrupted_memory, "mul\\(\\d{1,3},\\d{1,3}\\)", simplify = TRUE) |>
+    as.character()
+
+eval_mul_instruction <- function(string) {
+    is_valid <- str_detect(string, "mul\\(\\d{1,3},\\d{1,3}\\)")
+    if (!is_valid) stop("Invalid mul instruction")
+
+    numbers <- str_extract_all(string, "\\d{1,3}", simplify = TRUE) |> as.numeric()
+
+    result <- prod(numbers)
+
+    return(result)
+}
+
+mul_results <- map_int(mul_instructions, eval_mul_instruction)
+
+sum(mul_results)
